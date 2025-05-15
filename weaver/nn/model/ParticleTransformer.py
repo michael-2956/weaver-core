c3a709000212268f945d2892bad4dc986e491ae4
michael-2956 <work.michael.2956@gmail.com>
2025-05-15 16:54:05 +0300

    finish assing sink toke

--- weaver/nn/model/ParticleTransformer.py
+++ weaver/nn/model/ParticleTransformer.py
@@ -876,9 +876,12 @@ class ParticleTransformer(nn.Module):
         trunc_normal_(self.cls_token, std=.02)
 
         self.sink_token = None
+        self.sink_token_v = None
         if add_sink_token:
             self.sink_token = nn.Parameter(torch.zeros(1, 1, embed_dim), requires_grad=True)
-            trunc_normal_(self.cls_token, std=.02)
+            trunc_normal_(self.sink_token, std=.02)
+            self.sink_token_v = nn.Parameter(torch.zeros(1, pair_input_dim, 1), requires_grad=True)
+            trunc_normal_(self.sink_token_v, std=.02)
 
     @torch.jit.ignore
     def no_weight_decay(self):
@@ -907,7 +910,10 @@ class ParticleTransformer(nn.Module):
                     padding_mask = torch.cat((  # sink is not padding, add zeros
                         torch.zeros_like(padding_mask[:, :1]), padding_mask
                     ), dim=1)
-                x = torch.cat((self.sink_token, x), dim=0)
+                sink_tokens = self.sink_token.expand(1, x.size(1), -1)  # (1, N, C)
+                x = torch.cat((sink_tokens, x), dim=0)  # (P + 1, N, C)
+                sink_tokens_v = self.sink_token_v.expand(v.size(0), -1, 1)  # (N, 4, 1)
+                v = torch.cat([sink_tokens_v, v], dim=2)  # (N, 4, P + 1)
 
             if self.return_qk_final_U_attn_weights:
                 attn_weights_list = []
