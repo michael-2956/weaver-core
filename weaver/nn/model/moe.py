ccbc125d15e065bfdbe34f2f7e6221d9bf78a0aa
Oleh Prostakov <prostakov.oleh@gmail.com>
2025-05-25 14:19:47 +0300

    comments (1)

--- weaver/nn/model/moe.py
+++ weaver/nn/model/moe.py
@@ -27,45 +27,61 @@ MixtureOfExpertsReturn = namedtuple('MixtureOfExpertsReturn', [
     'total_aux_loss'
 ])
 
-# helper functions
-
+# ==================================== HELPER FUNCTIONS ====================================
 def exists(val):
     return val is not None
 
 def default(val, default):
     if exists(val):
         return val
-
     return default() if callable(default) else default
 
 def divisible_by(num, den):
     return (num % den) == 0
 
 def chunk_num(num, chunks):
+    """
+    Get sizes of each chunk
+    chunk_num(10, 3) -> [4, 3, 3]
+    """
     num_per_chunk, remainder = divmod(num, chunks)
-
     out = []
     for i in range(chunks):
         n = num_per_chunk
         out.append(n + int(i < remainder))
-
     return out
 
 def pack_one(t, pattern):
+    """
+    Convenience wrapper around `einops.pack` for a single tensor.
+    Args:
+        t (Tensor): input tensor to be packed.
+        pattern (str): einops pattern describing the packing transformation.
+    Returns:
+        Tuple[Tensor, Any]: the packed tensor and metadata for unpacking.
+    """
     return pack([t], pattern)
 
 def unpack_one(t, ps, pattern):
+    """
+    Convenience wrapper around `einops.unpack` for a single tensor.
+    """
     return unpack(t, ps, pattern)[0]
 
 def cast_tuple(el, len = 1):
+    """
+    Check if 'el' is a tuple, and if not, cast it to a tuple of length 'len' containing 'el'
+    """
     return el if isinstance(el, tuple) else ((el,) * len)
 
 def Sequential(*modules):
     return nn.Sequential(*filter(exists, modules))
 
-# tensor related helper functions
-
+# ============================= TENSOR-RELATED HELPER FUNCTIONS =======================
 def cumsum_exclusive(t, dim = -3):
+    """
+    Get cumulative sum of all elements BEFORE the current one. Works for more than 1 dim
+    """
     assert dim < 0
     num_pad_dims = -dim - 1
     pre_padding = (0, 0) * num_pad_dims
@@ -78,15 +94,15 @@ def gumbel_noise(t):
     noise = torch.zeros_like(t).uniform_(0, 1)
     return -log(-log(noise))
 
-# pytorch one hot throws an error if there are out of bound indices.
-# tensorflow, in contrast, does not throw an error
-
 def safe_one_hot(indexes, max_length):
+    """
+    PyTorch one hot throws an error if there are out of bound indices.
+    Tensorflow, in contrast, does not
+    """
     max_index = indexes.max() + 1
     one_hot_classes = max(max_index + 1, max_length)
     return F.one_hot(indexes, one_hot_classes)[..., :max_length]
 
-# rms normalization
 class RMSNorm(Module):
     def __init__(self, dim):
         super().__init__()
@@ -96,9 +112,10 @@ class RMSNorm(Module):
     def forward(self, x):
         return F.normalize(x, dim = -1) * self.gamma * self.scale
 
-# expert class
-# best performing was ff geglu with multiplicative bias (just after gating)
 class GEGLU(Module):
+    """
+    GEGLU works better than GELU, according to the authors
+    """
     def __init__(
         self,
         dim,
@@ -112,6 +129,9 @@ class GEGLU(Module):
         return F.gelu(gate) * x * self.mult_bias
 
 class Expert(Module):
+    """
+    An 'Expert' feedforward network
+    """
     def __init__(self,
                  embed_dim,
                  ffn_dim,
@@ -122,7 +142,7 @@ class Expert(Module):
         super().__init__()
 
         if prenorm:
-            self.norm = RMSNorm(embed_dim)
+            self.pre_fc_norm = RMSNorm(embed_dim)
 
         self.act = GEGLU(ffn_dim) if activation == 'gelu' else nn.ReLU()
         self.act_dropout = nn.Dropout(activation_dropout)
@@ -132,6 +152,9 @@ class Expert(Module):
         self.apply(self.init_)
 
     def init_(self, module):
+        """
+        Uniform init values
+        """
         if isinstance(module, nn.Linear):
             dim = module.weight.shape[0]
             std = dim ** -0.5
@@ -140,6 +163,8 @@ class Expert(Module):
             module.bias.data.uniform_(-std, std)
 
     def forward(self, x):
+        if self.pre_fc_norm is not None:
+            x = self.pre_fc_norm(x)
         x = self.fc1(x)
         x = self.act(x)
         x = self.act_dropout(x)
@@ -161,28 +186,6 @@ class Experts(Module):
 
         self.allow_var_seq_len = allow_var_seq_len
 
-        # device tracker, since need to manually move experts not in use to CPU in distributed
-
-        self.register_buffer('dummy', torch.ones(1), persistent = False)
-
-    @property
-    def device(self):
-        return self.dummy.device
-
-    def all_experts_to_cpu_besides(self, selection):
-        if isinstance(selection, int):
-            experts = [self.experts[selection]]
-        if isinstance(selection, slice):
-            experts = self.experts[selection]
-        else:
-            experts = selection
-
-        experts_set = set(experts)
-
-        for expert in self.experts:
-            device = self.device if expert in experts_set else 'cpu'
-            expert.to(device)
-
     def forward(self, x):
         """
         einops notation:
@@ -193,23 +196,23 @@ class Experts(Module):
         """
 
         shape, num_experts = x.shape, self.num_experts
+        # set the expert dimension first, so torch can fuse calls-per-expert
         x = rearrange(x, 'b e n d -> e b n d')
         outs = []
 
+        # call each expert sequentially
         for expert, expert_input in zip(self.experts, x):
             out = expert(expert_input)
             outs.append(out)
 
+        # gather outputs from all experts
         if len(outs) > 0:
             outs = torch.stack(outs)
         else:
             outs = torch.empty_like(x, requires_grad = self.training)
 
-        # all gather across merged expert batches dimensions
-        # then split the batch dimension back
-
+        # split the batch dimension back first
         outs = rearrange(outs, 'e b n d -> b e n d')
-
         assert outs.shape == shape
         return outs
 
@@ -281,58 +284,48 @@ class TopNGating(Module):
         c - capacity
         """
 
+        # x arrives as [*, b, n, dim]
         *_, b, group_size, dim, dtype, top_n, num_gates, eps = *x.shape, x.dtype, self.top_n, self.num_gates, self.eps
 
         # threshold, capacity depending on training or eval
-
         suffix = 'train' if self.training else 'eval'
-
         threshold = getattr(self, f'threshold_{suffix}')
         capacity_factor = getattr(self, f'capacity_factor_{suffix}')
 
         # Each sequence sends (at most?) expert_capacity positions to each expert.
         # Static expert_capacity dimension is needed for expert batch sizes
-
         expert_capacity = min(group_size, int((group_size * capacity_factor) / num_gates))
         expert_capacity = max(expert_capacity, MIN_EXPERT_CAPACITY)
         expert_capacity_f = float(expert_capacity)
 
         # gate logits and gates
-
         gate_logits = self.to_gates(x)
-
         maybe_noised_gate_logits = gate_logits
-
         if noise_gates:
             noise = gumbel_noise(maybe_noised_gate_logits)
             maybe_noised_gate_logits = maybe_noised_gate_logits + noise * noise_mult
 
+        # find top N experts per token
         raw_gates = maybe_noised_gate_logits.softmax(dim = -1) # [b, n, e]
-
-        # find top N experts per position
-
         topk_return = self.topk(raw_gates, k = top_n) # [b, n, k]
-
         gate_indices = topk_return.indices
-
         if self.differentiable_topk:
             # allow for differentiable topk using coordinate descent
             # used successfully for routing from CoLT5 paper https://github.com/lucidrains/CoLT5-attention
-
             gates = topk_return.coor_descent_values
         else:
             gates = topk_return.values
 
-        # move the top-n dimension to be first
+        # move the top-n dimension to be first, so experts are easier to batch
         gates = rearrange(gates, '... k -> k ...') # [k, b, n]
         gate_indices = rearrange(gate_indices, '... k -> k ...') # [k, b, n]
 
         # masks
-        one_hot_gate_indices = F.one_hot(gate_indices, num_gates)
+        one_hot_gate_indices = F.one_hot(gate_indices, num_gates) # [k, b, n, e], where k - top-Kth expert, e - 1-hot vector with '1' at the index of top-Kth expert and 0s elsewhere
         mask = one_hot_gate_indices.float()
         mask_1 = mask[0] # needed for balancing loss
 
-        # normalize top-n gate scores
+        # re-normalize top-k (because some values were masked)
         denom = reduce(gates, 'k ... -> 1 ...', 'sum').clamp(min = eps)
         gates = gates / denom
 
@@ -353,30 +346,27 @@ class TopNGating(Module):
         positions = []
         prev_expert_count = 0.
 
+        # Iterate over top_n positions
         for n in range(self.top_n):
+            # Find out top-Nth experts for each token position
             position_in_expert = (mask_cumsum[n] + prev_expert_count) * mask[n]
 
-            # Remove the elements that don't fit. (batch, sequence, experts)
+            # Remove the elements that don't fit (never applicable for the top-1 expert)
             mask[n] *= (position_in_expert < expert_capacity_f).float()
 
-            # How many examples in this sequence go to this expert - needed for the next iteration as offset
+            # Find out how many tokens so far have come to each expert
             prev_expert_count = reduce(mask[n], '... n e -> ... 1 e', 'sum') + prev_expert_count
 
-            # (batch, sequence)
-            position_in_expert = reduce(position_in_expert, '... n e -> ... n', 'sum')
+            # compute how many tokens have already been routed to every expert
+            position_in_expert = reduce(position_in_expert, '... n e -> ... n', 'sum') # [b, n]
             positions.append(position_in_expert)
 
         positions = torch.stack(positions)
 
-        # (k, batch, sequence) - mostly ones, but zeros where something didn't fit
-        mask_flat = reduce(mask, '... n e -> ... n', 'sum')
-
-        # (k, batch, sequence) - weighted assignment
-        # following https://github.com/tensorflow/mesh/blob/master/mesh_tensorflow/transformer/moe.py#L1903
-        gates = gates * mask_flat
+        # mostly ones, but zeros where something didn't fit
+        mask_flat = reduce(mask, '... n e -> ... n', 'sum') # [k, b, n]
 
         # (batch, sequence, experts, expert_capacity)
-
         combine_tensor = einx.multiply(
             'k b n, k b n, k b n e, k b n c -> k b n e c',
             gates,
@@ -386,9 +376,6 @@ class TopNGating(Module):
         )
 
         combine_tensor = reduce(combine_tensor, 'k b n e c -> b n e c', 'sum')
-
-        # dispatch tensor
-
         dispatch_tensor = combine_tensor.bool().type(dtype)
 
         if self.straight_through_dispatch_tensor:
